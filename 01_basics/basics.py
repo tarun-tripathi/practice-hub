8ea895b36914d63eb6c5018e316b898bcc62dc81
tarun-tripathi <taruntripathi7654@gmail.com>
2026-04-26 18:22:01 +0530

    Q1: Student marks analyser completed

---
+++ 01_basics/basics.py
@@ -0,0 +1,10 @@
+def analyse_marks(marks):
+    avg = sum(marks) / len(marks)
+    highest = max(marks)
+    lowest = min(marks)
+    results = ["Pass" if m >= 40 else "Fail" for m in marks]
+    return avg, highest, lowest, results
+
+# Test
+marks = [45, 78, 32, 90, 55]
+print(analyse_marks(marks))
